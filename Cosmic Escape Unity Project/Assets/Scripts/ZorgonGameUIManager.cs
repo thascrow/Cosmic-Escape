4555e2f2a13a3c6865f0016f059c18a3241b09c3
omarlatreche <omar1933@hotmail.co.uk>
2022-02-20 17:01:42 +0000

    File organisation, begun work on asteroid game
    - added move functionality to player
    - added timer and points functionality

---
+++ Cosmic Escape Unity Project/Assets/Scripts/ZorgonGameUIManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ZorgonGameUIManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timeText;
+    [SerializeField] private TextMeshProUGUI pointsText;
+    private float countdownTimer;
+    [SerializeField] private float timeLimit;
+    private bool timing;
+    [SerializeField] private GameManager gameManager;
+
+    private void Start()
+    {
+        timing = true;
+        countdownTimer = timeLimit;
+    }
+
+    private void Update()
+    {
+        UpdateTimer();
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        pointsText.text = gameManager.points.ToString("00.00");
+    }
+
+    void UpdateTimer()
+    {
+        if (countdownTimer > 0 && timing)
+        {
+            countdownTimer -= Time.deltaTime;
+            timeText.text = countdownTimer.ToString("0.00");
+        }
+        else if (countdownTimer <= 0)
+        {
+            timing = false;
+            gameManager.GameOver();
+            // Select winner
+            // give rewards
+            // Move onto mainlevel
+        }
+    }
+}
