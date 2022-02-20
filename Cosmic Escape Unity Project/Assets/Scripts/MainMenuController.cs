1b7bbe15b65cfe352c312d9c58e1eda3d1ea5d65
Bowman <mb6122e@gre.ac.uk>
2022-02-20 19:24:44 +0000

    MainMenu
    sada

--- Cosmic Escape Unity Project/Assets/Scripts/MainMenuController.cs
+++ Cosmic Escape Unity Project/Assets/Scripts/MainMenuController.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] GameObject mainMenu, optionMenu;
+    
+    public void Option()
     {
-        
+        mainMenu.SetActive(false);
+        optionMenu.SetActive(true);
+    }
+   public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+   public void ExitButton()
+    {
+        Application.Quit();
     }
 }
