55ffc541cef2bfa8705096ee166a245df740b619
omarlatreche <omar1933@hotmail.co.uk>
2022-02-21 13:41:18 +0000

    Asteroid mini game improvements
    - camera improvements
    - asteroids destroyed on collision with floor

---
+++ Cosmic Escape Unity Project/Assets/Scripts/ExplodeOnCollision.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeOnCollision : MonoBehaviour
+{
+    [SerializeField] private string triggerObject;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision != null)
+        {
+            if (collision.gameObject.name == triggerObject)
+            {
+                // explode effect
+
+                Destroy(gameObject);
+            }
+        }
+    }
+}
