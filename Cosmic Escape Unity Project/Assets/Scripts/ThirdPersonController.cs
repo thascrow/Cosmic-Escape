bdd646eef70cd12a59c241a18f2cf17d6446439d
mbowyergre <92578200+mbowyergre@users.noreply.github.com>
2022-03-11 15:31:49 +0000

    Controller
    Yoomao

--- Cosmic Escape Unity Project/Assets/Scripts/ThirdPersonController.cs
+++ Cosmic Escape Unity Project/Assets/Scripts/ThirdPersonController.cs
@@ -9,6 +9,7 @@ public class ThirdPersonController : MonoBehaviour
     private Vector3 playerVelocity;
     [SerializeField] private float playerSpeed;
     [SerializeField] private float mouseSensitivity;
+    [SerializeField] bool player1, player2, player3, player4;
     
     private void Start()
     {
@@ -18,13 +19,44 @@ public class ThirdPersonController : MonoBehaviour
     void Update()
     {
         // move player based on inputs
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-        controller.Move(playerVelocity * Time.deltaTime);
+        if (player1)
+        {
+            Vector3 move1 = new Vector3(Input.GetAxis("Horizontal1"), 0, Input.GetAxis("Vertical1"));
+            controller.Move(-move1 * Time.deltaTime * playerSpeed);
+            controller.Move(-playerVelocity * Time.deltaTime);
+            float mouseX = Input.GetAxis("XRotation1") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("YRotation1") * mouseSensitivity * Time.deltaTime;
+            gameObject.transform.Rotate(Vector3.up * mouseX);
+            gameObject.transform.Rotate(Vector3.up * mouseY);
+
+        }
+        /*
+        if (player2)
+        {
+            Vector3 move2 = new Vector3(Input.GetAxis("Horizontal2"), 0, Input.GetAxis("Vertical2"));
+            controller.Move(move2 * Time.deltaTime * playerSpeed);
+            controller.Move(-playerVelocity * Time.deltaTime);
+
+        }
+        */
+        if (Input.GetButton("XRotation1"))
+        {
+            print("Hello");
+            print("Hello");
+        }
+
+        if (Input.GetButton("YRotation1"))
+        {
+            print("Hello");
+            print("Hello");
+            print("Hello");
+        }
+
+
+
 
         // rotate player based on inputs
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        gameObject.transform.Rotate(Vector3.up * mouseX);
+        
 
         
     } 
